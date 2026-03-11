d8603753248ced9351d97800e9747345a10a3021
Elsie Fynn <256878@student.wqe.ac.uk>
2026-03-11 14:02:39 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace Grade_tracker_start
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("yay");
+        }
+    }
+}
